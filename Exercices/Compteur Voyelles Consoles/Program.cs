001c0a57b9e664505aa584fd87f3834942c823bf
fwillemain <fwillemain@FWILLEMAIN17-DE>
2017-03-23 13:25:31 +0100

    Ajout projet Compteur VC

---
+++ Exercices/Compteur Voyelles Consoles/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compteur_Voyelles_Consoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbrVoyelles, nbrConsonnes;
+
+            Console.Write("Entrez un mot : ");
+            string mot = Console.ReadLine();
+
+            CompteurVoyellesConsonnes(mot, out nbrVoyelles, out nbrConsonnes);
+            Console.WriteLine("\"{0}\" comprend {1} consonnes et {2} voyelles.", mot, nbrConsonnes, nbrVoyelles);
+
+            Console.ReadKey();
+
+        }
+
+        static void CompteurVoyellesConsonnes(string mot, out int nbrVoyelles, out int nbrConsonnes)
+        {
+            nbrVoyelles = 0;
+
+            mot = mot.ToLower();
+            
+            for(int i  = 0; i < mot.Length; i++)
+            {
+                if (EstVoyelle(mot[i]))
+                    nbrVoyelles++;
+            }
+
+            nbrConsonnes = mot.Length - nbrVoyelles;
+        }
+
+        static bool EstVoyelle(char c)
+        {
+            return (c.Equals('a') || c.Equals('e') || c.Equals('i') || c.Equals('o') || c.Equals('u') || c.Equals('y')); 
+        }
+
+    }
+}
---
+++ Exercices/Tri tableau/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tri_tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] tableau = { "C", "D", "A", "B", "F", "A"};
+
+            Console.WriteLine("Affichage tableau trié");
+            AfficherTableau(TrierTableau(tableau));
+
+            Console.WriteLine("Affichage tableau d'origine");
+            AfficherTableau(tableau);
+
+            Console.ReadKey();
+
+        }
+
+        static void AfficherTableau(string[] tableauAAfficher)
+        {
+            for (int i = 0; i < tableauAAfficher.Length; i++)
+                Console.Write(tableauAAfficher[i] + " ");
+            Console.Write('\n');
+        }
+
+        
+        static string[] TrierTableau(string[] tableauATrier)
+        {
+            string[] resultat = new string[tableauATrier.Length];
+
+            for (int i = 0; i < tableauATrier.Length; i++)
+                resultat[i] = tableauATrier[i];
+            
+            for (int i = 0; i < resultat.Length; i++)
+            {
+                int j = i;
+                while(j > 0 && (resultat[j - 1].CompareTo(resultat[j]) == 1))
+                {
+                    string temp = resultat[j - 1];
+                    resultat[j - 1] = resultat[j];
+                    resultat[j] = temp;
+                    j--;
+                }
+            }
+
+            return resultat;
+        }
+
+    }
+}
