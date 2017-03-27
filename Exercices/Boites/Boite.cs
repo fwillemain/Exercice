7eab02694ebaf84816aa9b3b418353fb46d4dd34
fwillemain <fwillemain@FWILLEMAIN17-DE>
2017-03-27 10:55:21 +0200

    Commit & push du projet Boite

---
+++ Exercices/Boites/Boite.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boites
+{
+    public enum Couleurs { Blanc, Bleu, Vert, Jaune, Orange, Rouge, Marron }
+    public enum Matières { Carton, Plastique, Bois, Métal }
+
+    public class Boite
+    {    
+        #region Champs privés
+        private double _hauteur;
+        private double _largeur;
+        private double _longueur;
+        #endregion
+
+        #region Propriétés
+        public Couleurs Couleur { get; set; }
+
+        public Matières Matière { get; private set; }
+
+        public double Volume
+        {
+            get { return _largeur * _longueur * _hauteur; }
+        }
+        #endregion
+
+        #region Constructeurs
+        public Boite()
+        {
+            _hauteur = 30;
+            _largeur = 30;
+            _longueur = 30;
+            Matière = Matières.Carton;
+        }
+
+        #endregion
+
+        #region Méthodes publiques
+        public string Etiqueter(string destinataire)
+        {
+            throw new NotImplementedException();
+            //TODO : ajouter des fonctionnalités
+        }
+
+        public string Etiqueter(string destinataire, bool fragile)
+        {
+            throw new NotImplementedException();
+            //TODO : ajouter des fonctionnalités
+
+        }
+
+        #endregion
+
+
+
+
+
+    }
+}
--- Exercices/Boites/Program.cs
+++ Exercices/Boites/Program.cs
@@ -10,73 +10,9 @@ namespace Boites
     {
         static void Main(string[] args)
         {
-
+            Boite test = new Boite();
+            
             //TODO : utiliser la classe Boite
-
-        }
-
-        public class Boite
-        {
-            public enum Couleur { Blanc, Bleu, Vert, Jaune, Orange, Rouge, Marron }
-            public enum Matière { Carton, Plastique, Bois, Métal }
-
-            #region Champs privés
-            private double _hauteur;
-            private double _largeur;
-            private double _longueur;
-            private Couleur _couleurBoite;
-            private Matière _matièreBoite;
-            #endregion
-
-            #region Propriétés
-            public Couleur CouleurBoite
-            {
-                get { return _couleurBoite; }
-                set { _couleurBoite = value; }
-            }
-
-            public Matière MatièreBoite
-            {
-                get { return _matièreBoite; }
-            }
-
-            public double Volume
-            {
-                get { return _largeur * _longueur * _hauteur; }
-            }
-            #endregion
-
-            #region Constructeurs
-            public Boite()
-            {
-                _hauteur = 30;
-                _largeur = 30;
-                _longueur = 30;
-                _matièreBoite = Matière.Carton;
-            }
-
-            #endregion
-
-            #region Méthodes publiques
-            public string Etiqueter(string destinataire)
-            {
-                throw new NotImplementedException();
-                //TODO : ajouter des fonctionnalités
-            }
-
-            public string Etiqueter(string destinataire, bool fragile)
-            {
-                throw new NotImplementedException();
-                //TODO : ajouter des fonctionnalités
-
-            }
-
-            #endregion
-
-
-
-
-
 
         }
 
