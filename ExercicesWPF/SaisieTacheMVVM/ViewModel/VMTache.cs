1167db7230a72f709be219edf1f1645473ef43c5
fwillemain <fwillemain@FWILLEMAIN17-DE>
2017-05-18 10:37:09 +0200

    Mise en forme de VMTache.cs

--- ExercicesWPF/SaisieTacheMVVM/ViewModel/VMTache.cs
+++ ExercicesWPF/SaisieTacheMVVM/ViewModel/VMTache.cs
@@ -14,67 +14,17 @@ namespace SaisieTacheMVVM.ViewModel
 
     public class VMTache : ViewModelBase
     {
+        #region Champs privés
+        private ICommand _ajouterTache;
+        private ICommand _supprimerTache;
+        private ICommand _enregistrerTache;
+        private ICommand _annulerTache;
+        private Tache _tacheCourante;
+        private ModesEdition _modeEditionCourant;
+        #endregion
+
+        #region Propriétés
         public ObservableCollection<Tache> ListeTache { get; set; }
-
-        public VMTache()
-        {
-            ListeTache = new ObservableCollection<Tache>(DAL.RécupérerListeTache());
-        }
-
-        ICommand _ajouterTache;
-        ICommand _supprimerTache;
-        ICommand _enregistrerTache;
-        ICommand _annulerTache;
-        Tache _tacheCourante;
-        ModesEdition _modeEditionCourant;
-
-        // TODO : revoir RelayCommand et la création des commandes dans le Context pour être à l'aise
-        public ICommand CommandeAjouterTache
-        {
-            get
-            {
-                if (_ajouterTache == null)
-                    _ajouterTache = new RelayCommand(AjouterTache);
-
-                return _ajouterTache;
-            }
-        }
-        public ICommand CommandeSupprimerTache
-        {
-            get
-            {
-                if (_supprimerTache == null)
-                    _supprimerTache = new RelayCommand(SupprimerTache);
-
-                return _supprimerTache;
-            }
-        }
-        public ICommand CommandeEnregistrerTache
-        {
-            get
-            {
-                if (_enregistrerTache == null)
-                    _enregistrerTache = new RelayCommand(EnregistrerTache, ActiverBouton);
-
-                return _enregistrerTache;
-            }
-        }
-        public ICommand CommandeAnnulerTache
-        {
-            get
-            {
-                if (_annulerTache == null)
-                    _annulerTache = new RelayCommand(AnnulerCommande, ActiverBouton);
-
-                return _annulerTache;
-            }
-        }
-
-        private bool ActiverBouton(object obj)
-        {
-            return _modeEditionCourant == ModesEdition.Edition;
-        }
-
         public Tache TacheCourante
         {
             get { return _tacheCourante ?? ListeTache.FirstOrDefault(); }
@@ -91,7 +41,26 @@ namespace SaisieTacheMVVM.ViewModel
                 SetProperty(ref _modeEditionCourant, value);
             }
         }
+        #endregion
 
+        #region Constructeurs
+        public VMTache()
+        {
+            ListeTache = new ObservableCollection<Tache>(DAL.RécupérerListeTache());
+        }
+        #endregion
+
+        #region Commandes
+        public ICommand CommandeAjouterTache
+        {
+            get
+            {
+                if (_ajouterTache == null)
+                    _ajouterTache = new RelayCommand(AjouterTache);
+
+                return _ajouterTache;
+            }
+        }
         private void AjouterTache(object obj)
         {
             var tache = new Tache()
@@ -107,21 +76,58 @@ namespace SaisieTacheMVVM.ViewModel
             ModeEditionCourant = ModesEdition.Edition;
         }
 
+        public ICommand CommandeSupprimerTache
+        {
+            get
+            {
+                if (_supprimerTache == null)
+                    _supprimerTache = new RelayCommand(SupprimerTache);
+
+                return _supprimerTache;
+            }
+        }
         private void SupprimerTache(object obj)
         {
             ListeTache.Remove(TacheCourante);
         }
 
+        public ICommand CommandeEnregistrerTache
+        {
+            get
+            {
+                if (_enregistrerTache == null)
+                    _enregistrerTache = new RelayCommand(EnregistrerTache, ActiverBouton);
+
+                return _enregistrerTache;
+            }
+        }
         private void EnregistrerTache(object obj)
         {
             ModeEditionCourant = ModesEdition.Consultation;
             DAL.EnregistrerListeTache(ListeTache.ToList());
         }
 
+        public ICommand CommandeAnnulerTache
+        {
+            get
+            {
+                if (_annulerTache == null)
+                    _annulerTache = new RelayCommand(AnnulerCommande, ActiverBouton);
+
+                return _annulerTache;
+            }
+        }
         private void AnnulerCommande(object obj)
         {
             ListeTache.Remove(TacheCourante);
             ModeEditionCourant = ModesEdition.Consultation;
         }
+
+        // CanExecute pour les commandes EnregistrerTache et AnnulerTache
+        private bool ActiverBouton(object obj)
+        {
+            return _modeEditionCourant == ModesEdition.Edition;
+        }
+        #endregion
     }
 }
